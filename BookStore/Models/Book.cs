d92b8bdf160141239b95b13025dd19b539eb8973
MungHtun <nawnawmunghtun@gmail.com>
2022-10-15 18:41:24 +0630

    update Book model

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -7,10 +7,8 @@ namespace BookStore.Models
         public int Id { get; set; }
         [Display(Name = "Book Title")]
         [Required]
-        [StringLength(maximumLength: 20, ErrorMessage = "The Title length should be between 2 and 20.", MinimumLength = 2)]
         public string Title { get; set; }
         public string Genre { get; set; }
-        public List<string> Authors { get; set; }
         [DataType(DataType.Currency)]
         [Range(1, 100)]
         public decimal Price { get; set; }
